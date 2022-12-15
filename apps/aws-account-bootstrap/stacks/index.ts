0d3b3133eebcd11c12190a3d3c60ca37331bc7fe
sionelt <sltafengatoto@gmail.com>
2022-12-15 16:01:19 -0700

    remove tags from bootstraps

--- apps/aws-account-bootstrap/stacks/index.ts
+++ apps/aws-account-bootstrap/stacks/index.ts
@@ -1,4 +1,3 @@
-import {Tags} from 'aws-cdk-lib'
 import {App} from 'sst/constructs'
 import {GithubOidc} from './github-oidc-stack'
 import {HostedZone} from './hosted-zone-stack'
@@ -6,7 +5,4 @@ import {Ses} from './ses-stack'
 
 export default function (app: App) {
   app.stack(HostedZone).stack(GithubOidc).stack(Ses)
-
-  Tags.of(app).add('app', app.name)
-  Tags.of(app).add('stage', app.stage)
 }
